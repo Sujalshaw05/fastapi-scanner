76af6da0032451a8e50fccf9ede53b212f0a3b58
HindolKarfarma-learn <hindolkarfarma@gmail.com>
2026-04-16 23:41:12 +0530

    add qr_decode fuctionality, update scan route and simpliify main.py

--- app/routes/scan.py
+++ app/routes/scan.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, UploadFile, File
-
+from app.services.qr_decode import qr_decode_
 
 router = APIRouter()
 
@@ -11,7 +11,9 @@ async def health_check():
 
 @router.post("/scan-frame")
 async def scan_frame(file: UploadFile = File(...)):
+    
     # Person 2 will add image validation here
     # Person 3 will add QR detection here
+    massage= await qr_decode_(file)
+    return massage
     # Person 4 will add URL resolution and DB saving here
-    pass
