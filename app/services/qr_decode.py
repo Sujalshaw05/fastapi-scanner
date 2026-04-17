304625f1e707b55b11aa9e67220f627407e86d55
HindolKarfarma-learn <hindolkarfarma@gmail.com>
2026-04-17 19:27:52 +0530

    intigrate all services

--- app/services/qr_decode.py
+++ app/services/qr_decode.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np
-async def qr_decode_(file):
-    contents = await file.read()
-    np_arr = np.frombuffer(contents, np.uint8)
-    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+async def qr_decode_(img):
+    # contents = await file.read()
+    # np_arr = np.frombuffer(contents, np.uint8)
+    # img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
     
     detector = cv2.QRCodeDetector()
     data, bbox, _ = detector.detectAndDecode(img)
 
     if data:
-        return {"data": data}
+        return data
     else:
-        return {"data": None}
+        return None
